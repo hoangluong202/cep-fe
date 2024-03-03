980f650f31117499d4743ea842de378654b2c9da
hoangluong202 <hl01012002@gmail.com>
2024-03-04 01:26:50 +0700

    feat(map): update many smartpole in map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,12 +1,57 @@
 import { APIProvider, Map, InfoWindow, useMarkerRef, Marker } from '@vis.gl/react-google-maps';
 import { useState } from 'react';
 import { Card, CardBody, CardFooter, Typography, Button } from '@material-tailwind/react';
-// const HCMUT_CORNERS = [
-//   { lat: 10.770692797316515, lng: 106.65826141343352 },
-//   { lat: 10.774569690082894, lng: 106.6620894625359 },
-//   { lat: 10.77634012117674, lng: 106.66002415769374 },
-//   { lat: 10.772852090456192, lng: 106.6575775799744 }
-// ];
+
+const HCMUT_CORNERS = [
+  { lat: 10.770692797316515, lng: 106.65826141343352 },
+  { lat: 10.774569690082894, lng: 106.6620894625359 },
+  { lat: 10.77634012117674, lng: 106.66002415769374 },
+  { lat: 10.772852090456192, lng: 106.6575775799744 },
+  { lat: 10.77404592132156, lng: 106.65984371362426 }
+];
+
+interface MarkerWithInfoProps {
+  position: { lat: number; lng: number };
+  key: number;
+}
+
+const MarkerWithInfo: React.FC<MarkerWithInfoProps> = ({ position }) => {
+  const [infowindowOpen, setInfowindowOpen] = useState(false);
+  const [markerRef, marker] = useMarkerRef();
+  return (
+    <>
+      <Marker
+        ref={markerRef}
+        position={position}
+        icon={{
+          url: 'https://pic.onlinewebfonts.com/thumbnails/icons_709501.svg',
+          scaledSize: { width: 25, height: 25, equals: () => true }
+        }}
+        onMouseOver={() => setInfowindowOpen(true)}
+        onMouseOut={() => setInfowindowOpen(false)}
+      />
+
+      {infowindowOpen && (
+        <InfoWindow anchor={marker}>
+          <Card className='mt-6 w-96'>
+            <CardBody>
+              <Typography variant='h5' color='blue-gray' className='mb-2'>
+                UI/UX Review Check
+              </Typography>
+              <Typography>
+                The place is close to Barceloneta Beach and bus stop just 2 min by walk and near to
+                &quot;Naviglio&quot; where you can enjoy the main night life in Barcelona.
+              </Typography>
+            </CardBody>
+            <CardFooter className='pt-0'>
+              <Button>Read More</Button>
+            </CardFooter>
+          </Card>
+        </InfoWindow>
+      )}
+    </>
+  );
+};
 
 export function SimpleMap() {
   const defaultProps = {
@@ -17,9 +62,6 @@ export function SimpleMap() {
     zoom: 16
   };
 
-  const [markerRef, marker] = useMarkerRef();
-  const [infowindowOpen, setInfowindowOpen] = useState(false);
-
   return (
     // Important! Always set the container height explicitly
     <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_KEY}>
@@ -29,35 +71,9 @@ export function SimpleMap() {
         center={defaultProps.center}
         mapId={'key'}
       >
-        <Marker
-          ref={markerRef}
-          position={defaultProps.center}
-          icon={{
-            url: 'https://pic.onlinewebfonts.com/thumbnails/icons_709501.svg',
-            scaledSize: { width: 25, height: 25, equals: () => true }
-          }}
-          onMouseOver={() => setInfowindowOpen(true)}
-          onMouseOut={() => setInfowindowOpen(false)}
-        />
-
-        {infowindowOpen && (
-          <InfoWindow anchor={marker}>
-            <Card className='mt-6 w-96'>
-              <CardBody>
-                <Typography variant='h5' color='blue-gray' className='mb-2'>
-                  UI/UX Review Check
-                </Typography>
-                <Typography>
-                  The place is close to Barceloneta Beach and bus stop just 2 min by walk and near
-                  to &quot;Naviglio&quot; where you can enjoy the main night life in Barcelona.
-                </Typography>
-              </CardBody>
-              <CardFooter className='pt-0'>
-                <Button>Read More</Button>
-              </CardFooter>
-            </Card>
-          </InfoWindow>
-        )}
+        {HCMUT_CORNERS.map((corner, index) => (
+          <MarkerWithInfo key={index} position={corner} />
+        ))}
       </Map>
     </APIProvider>
   );
