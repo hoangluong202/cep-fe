40106e5502ec5a18fd3aecdd768b3de58b200998
hoangluong202 <hl01012002@gmail.com>
2024-03-11 20:36:25 +0700

    feat(map): clear all labels in map except road labels

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -20,6 +20,49 @@ const defaultProps = {
   zoom: 16
 };
 
+const defaultStyle = [
+  {
+    featureType: 'all',
+    elementType: 'all',
+    stylers: [{ saturation: '32' }, { lightness: '-3' }, { visibility: 'on' }, { weight: '1.18' }]
+  },
+  {
+    featureType: 'administrative',
+    elementType: 'labels',
+    stylers: [{ visibility: 'off' }]
+  },
+  {
+    featureType: 'landscape',
+    elementType: 'labels',
+    stylers: [{ visibility: 'off' }]
+  },
+  {
+    featureType: 'landscape.man_made',
+    elementType: 'all',
+    stylers: [{ saturation: '-70' }, { lightness: '14' }]
+  },
+  {
+    featureType: 'poi',
+    elementType: 'labels',
+    stylers: [{ visibility: 'off' }]
+  },
+  {
+    featureType: 'transit',
+    elementType: 'labels',
+    stylers: [{ visibility: 'off' }]
+  },
+  {
+    featureType: 'water',
+    elementType: 'all',
+    stylers: [{ saturation: '100' }, { lightness: '-14' }]
+  },
+  {
+    featureType: 'water',
+    elementType: 'labels',
+    stylers: [{ visibility: 'off' }, { lightness: '12' }]
+  }
+] as google.maps.MapTypeStyle[];
+
 interface MarkerWithInfoProps {
   position: { lat: number; lng: number };
   key: number;
@@ -58,8 +101,9 @@ export function SimpleMap() {
         style={{ width: '100%', height: '100vh' }}
         defaultZoom={defaultProps.zoom}
         defaultCenter={defaultProps.center}
-        mapId={'key'}
+        // mapId={'key'}
         mapTypeId='terrain'
+        styles={defaultStyle}
       >
         {HCMUT_CORNERS.map((corner, index) => (
           <MarkerWithInfo key={index} position={corner} />
