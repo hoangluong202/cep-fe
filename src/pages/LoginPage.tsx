f519afc44aa9e060dcb3ee7afda2df22e1752ccd
hoangluong202 <hl01012002@gmail.com>
2024-08-17 16:26:09 +0700

    feat(login): update new ui for login page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -5,7 +5,6 @@ import { UserCircleIcon, KeyIcon } from '@heroicons/react/24/outline';
 import { authService, server } from '@services';
 import { useUserQuery } from '@hooks';
 import { useMutation } from '@tanstack/react-query';
-// import { Navigate } from 'react-router-dom';
 import { NavigateFunction, useNavigate } from 'react-router-dom';
 
 export function LoginPage() {
@@ -14,15 +13,6 @@ export function LoginPage() {
     info: { refetch }
   } = useUserQuery();
 
-  // const { refetch } = useQuery({
-  //   queryKey: ['api/users/1'],
-  //   queryFn: () => userService.getInfo(1),
-  //   retry(failureCount, error: ResponseError) {
-  //     if (error.statusCode && error.statusCode >= 400 && error.statusCode < 500) return false;
-  //     return failureCount < 0;
-  //   }
-  // });
-
   const { register, handleSubmit } = useForm<LoginFormData>();
 
   const loginNormal = useMutation({
@@ -30,22 +20,16 @@ export function LoginPage() {
     mutationFn: async (data: LoginFormData) => {
       const response = await authService.login(data);
       const { access_token } = response;
-      console.log('access_token', access_token);
       localStorage.setItem('authToken', access_token);
       server.defaults.headers.common['Authorization'] = `Bearer ${access_token}`;
-      console.log('server.defaults.headers.common', server.defaults.headers.common);
       return response;
     }
   });
   const submit: SubmitHandler<LoginFormData> = async (data) => {
     try {
-      console.log('login');
       await loginNormal.mutateAsync(data);
       await refetch();
       toast.success('Login successfully!');
-
-      // console.log('authToken', authToken);
-      // console.log('Navigating to /map');
       navigate('/map');
     } catch (err) {
       const errorMessage = (err as ResponseError).message;
@@ -54,45 +38,57 @@ export function LoginPage() {
   };
 
   return (
-    <Card color='transparent' shadow={false}>
-      <Typography variant='h4' color='blue-gray'>
-        Login
-      </Typography>
-      <Typography color='gray' className='mt-1 font-normal'>
-        Enter your username and password.
-      </Typography>
-      <form className='mt-8 mb-2 w-72 md:w-80 max-w-screen-lg' onSubmit={handleSubmit(submit)}>
-        <div className='mb-4 flex flex-col gap-6'>
-          <Input
-            id='auth-username'
-            size='lg'
-            label='Username'
-            icon={<UserCircleIcon />}
-            {...register('username', {
-              required: true,
-              minLength: 5
-            })}
-            // type='email'
-            crossOrigin=''
-          />
-          <Input
-            id='auth-password'
-            type='password'
-            size='lg'
-            icon={<KeyIcon />}
-            label='Password'
-            {...register('password', {
-              required: true,
-              minLength: 8
-            })}
-            crossOrigin=''
-          />
-        </div>
+    <div className="h-screen grid grid-cols-3 gap-8 bg-center bg-cover bg-fixed bg-no-repeat bg-[url('/src/assets/background-login.jpg')]">
+      <div className='grid col-span-2 grid-rows-3 gap-4 ml-24'>
+        <p className='row-span-2 font-sans text-3xl font-bold text-white self-end'>
+          Hệ thống lập lịch chiếu sáng cho đèn đường thông minh
+        </p>
+        <p className='font-sans text-base text-white font-medium'>
+          Phục vụ cho việc lên kế hoạch chiếu sáng và giám sát điện năng tiêu thụ cho hệ thống đèn
+          đường thông minh. Sử dụng tiêu chuẩn NEMA cho đèn LED, kết nối thông qua mạng NB-IoT và
+          phát triển hệ thống lập lịch, giám sát theo mô hình Microservices.
+        </p>
+      </div>
 
-        <Button className='mt-6 bg-blue-500' fullWidth type='submit'>
-          Login
-        </Button>
-      </form>
-    </Card>
+      <Card className='mt-24' color='transparent' shadow={false}>
+        <Typography variant='h4' color='white'>
+          Đăng nhập
+        </Typography>
+        <p className='mt-1 font-normal text-white'>Nhập tài khoản và mật khẩu để đăng nhập</p>
+        <form className='mt-8 mb-2 w-72 md:w-80 max-w-screen-lg' onSubmit={handleSubmit(submit)}>
+          <div className='mb-4 flex flex-col gap-6'>
+            <Input
+              id='auth-username'
+              size='lg'
+              label='Tài khoản'
+              icon={<UserCircleIcon color='white' />}
+              {...register('username', {
+                required: true,
+                minLength: 5
+              })}
+              color='white'
+              crossOrigin=''
+            />
+            <Input
+              id='auth-password'
+              type='password'
+              size='lg'
+              icon={<KeyIcon color='white' />}
+              label='Mật khẩu'
+              color='white'
+              {...register('password', {
+                required: true,
+                minLength: 8
+              })}
+              crossOrigin=''
+            />
+          </div>
+
+          <Button className='mt-6 bg-blue-500' fullWidth type='submit'>
+            ĐĂNG NHẬP
+          </Button>
+        </form>
+      </Card>
+    </div>
   );
 }
